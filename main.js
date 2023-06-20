fc7bc39bb7084e8bc896c943a4fbf0246eb0d1e2
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-21 06:22:41 +1200

    JASON.stringify

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-//ES6 module JS destructuring - Object literal destructuring
+const todos = [
+  {
+    id: 1,
+    text: "take out trash",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: "Meeting with bobo",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: "Dentist appt",
+    isCompleted: true,
+  },
 
-const person = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-    hobbies: ['music', 'movies', 'sports'],
-    address:{
-        street:'50 main st',
-        city: 'Boston',
-        state:'MA'
-    }
-}
-
-const {firstName, lastName, address: {city}} =person;
-console.log (city);
+];
+const todoJSON=JSON.stringify(todos)
