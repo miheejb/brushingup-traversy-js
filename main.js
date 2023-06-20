a799efd88f2bbb3fda59a2d5e95712bb362fbe66
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-21 11:33:09 +1200

    DOM manipulation

--- main.js
+++ main.js
@@ -1,45 +1,9 @@
-//Object orient programming
+//DOM Document orient model
 
-//constructor function
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastname;
-  this.dob = new Date(dob);
-  this.getBirthYear = function () {
-    return this.dob.getFullYear();
-  };
-  this.getFullName = function () {
-    return `${firstname} ${lastName}`;
-  };
-}
+//Single element
 
-//using prototype
+document.querySelector('.className')
+document.getElementById('');
 
-Person.prototype.getBirthYear = function () {
-  return this.dob.getFullYear();
-};
-Person.prototype.getFullName = function () {
-  return `${firstname} ${lastName}`;
-};
-
-//Class
-//method is function inside of the class
-class Person {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastname;
-    this.dob = new Date(dob);
-  }
-  getBirthyear() {
-    return this.dob.getFullYear();
-  }
-  getFullName() {
-    return `${firstname} ${lastName}`;
-  }
-}
-
-//Instantiate object
-const person1 = new Person("John", "Doe", "4-3-1998");
-console.log;
-
-console.log(person1.getBirthYear);
+//Multiple elements
+console.log(document.querySelectorAll('.item'));
