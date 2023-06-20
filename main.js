ab9e57baaa362ae32bb4f32ee26cb647694fa6f1
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-21 06:43:49 +1200

    forEach, map, filter

--- main.js
+++ main.js
@@ -16,4 +16,20 @@ const todos = [
   },
 
 ];
-const todoJSON=JSON.stringify(todos)
+//forEach, map(returns in array), filter
+
+todos.forEach(function(todo){
+    return console.log(todo.text);
+})
+
+const todoText = todo.map(function(todo){
+    return todo.text;
+})
+
+const todoCompleted = todos.filter(function(todo){
+    return todos.isCompleted ===true;
+});
+
+console.log(todoCompleted);
+
+ 
