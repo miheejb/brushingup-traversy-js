3fc2d1ff37eed3e87a7d4e65ccdc2df38cabfd1b
Mihee Jeong-Burns <mihee.jeong@gmail.com>
2023-06-21 14:29:42 +1200

    dom manipuation- submitting form/ background color

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-const ul = document.querySelector('.items');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector("#name");
+const emailinput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
-ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'brad';
-ul.lastElementChild.innerHTML = '<h1>hello</h1>'
+myForm. addEventListener('submit',onSubmit);
 
+function onSubmint(e){
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailinput.value ===''){
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields'
+
+        setTimeout(()=> msg.remove, 3000)
+    } else{ 
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`{nameInput.value}: ${emailInput.value}`));
+     }
+     userList.appendChild(li)
+
+    console.log(nameInput.value);
+
+    //clear fields
+    nameInput.value='';
+    emailInput.value=''
+}
+
+///toggle dark background
 const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.item').lastElementChild,innerHTML=''
+});
